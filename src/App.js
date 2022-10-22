3eb367efc395c2689c72362499dd5c7de2cf7db5
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-10-22 00:35:20 -0600

    Implement increment, decrement, add on a counter value by redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrement = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const addBy10 = () => {
+    dispatch({ type: "add", payload: 10 });
+  };
+
   return (
     <div className="App">
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy10}>Add by 10</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,19 @@
 import { createStore } from 'redux';
 
-const reducerFunction = (state = {counter: 10}, action) => {
+const reducerFunction = (state = { counter: 0 }, action) => {
+
+  if (action.type === "increment") {
+    return { counter: state.counter + 1 };
+  }
+
+  if (action.type === "decrement") {
+    return { counter: state.counter - 1 };
+  }
+
+  if (action.type === "add") {
+    return { counter: state.counter + action.payload };
+  }
+
   return state;
 };
 
